53b22323dbe39a79a8b797299c073760f8aef450
Zuqi Li <lizuqi1995@gmail.com>
2023-03-21 18:54:37 +0100

    Update basic_MVAE_pytorch.py
    Adapt the code to PyTorch Lightning

--- Scripts/basic_MVAE_pytorch.py
+++ Scripts/basic_MVAE_pytorch.py
@@ -1,34 +1,18 @@
-# This is the PyTorch version of the X-VAE implementation
+# This is the PyTorch Lightning version of the X-VAE implementation
 # Source: https://github.com/CancerAI-CL/IntegrativeVAEs/blob/master/code/models/xvae.py
 # Paper: https://www.frontiersin.org/articles/10.3389/fgene.2019.01205
+# PyTorch Lightning example: https://towardsdatascience.com/beginner-guide-to-variational-autoencoders-vae-with-pytorch-lightning-13dbc559ba4b
 
-from keras import backend as K
-from keras import optimizers
-from keras.layers import BatchNormalization as BN, Concatenate, Dense, Input, Lambda,Dropout
-from keras.models import Model
 
-from keras.losses import mean_squared_error,binary_crossentropy
-
+import os
 import torch
-from torch.utils.data import TensorDataset, DataLoader
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import models
-
+import torch.nn.functional as F
+import lightning as L
+from pytorch_lightning import Trainer
 import numpy as np
-import tensorflow as tf
-from tensorflow import set_random_seed
 
-
-def sse(true, pred):
-    return K.sum(K.square(true - pred), axis=1)
-
-def cce(true, pred):
-    return K.mean(K.sparse_categorical_crossentropy(true, pred, from_logits=True), axis=1)
-
-def bce(true, pred):
-    return K.sum(K.binary_crossentropy(true, pred, from_logits=True), axis=1)
 
 def compute_kernel(x, y):
     x_size = K.shape(x)[0] #K.shape(x)[0] #need to fix to get batch size
@@ -36,7 +20,7 @@ def compute_kernel(x, y):
     dim = K.int_shape(x)[1] #K.get_shape(x)[1] #x.get_shape().as_list()[1]   
     tiled_x = K.tile(K.reshape(x,K.stack([x_size, 1, dim])), K.stack([1,y_size, 1]))
     tiled_y = K.tile(K.reshape(y,K.stack([1, y_size, dim])), K.stack([x_size, 1, 1]))
-    kernel_input = K.exp(-K.mean((tiled_x - tiled_y)**2, axis=2)) / K.cast(dim, tf.float32)
+    kernel_input = K.exp(-K.mean((tiled_x - tiled_y)**2, axis=2)) / K.cast(dim, np.float32)
     return kernel_input
 
 def mmd(x, y):
@@ -45,123 +29,131 @@ def mmd(x, y):
     xy_kernel = compute_kernel(x, y)
     mmd = K.mean(x_kernel) + K.mean(y_kernel) - 2*K.mean(xy_kernel)
     return mmd
-    
-def sampling(args):
-    """Reparameterization trick by sampling fr an isotropic unit Gaussian.
-    # Arguments:
-        args (tensor): mean and log of variance of Q(z|X)
-    # Returns:
-        z (tensor): sampled latent vector
-    """
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    # by default, random_normal has mean=0 and std=1.0
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
-def kl_regu(z_mean,z_log_sigma):
+def kld(mu, log_var):
     #regularizer. this is the KL of q(z|x) given that the 
     #distribution is N(0,1) (or any known distribution)
-    kl_loss = 1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    return kl_loss
-  
-  
-class XVAE:
-    def __init__(self, args):
-        self.args = args
-        self.vae = None
-        self.encoder = None
+    kld = 1 + log_var - mu**2- torch.exp(log_var)
+    kld = kld.sum(dim = 1)
+    kld *= -0.5
+    return kld.mean(dim=0)
 
-    def build_model(self):
+    
+class XVAE(L.LightningModule):
+    def __init__(self, x1_size, x2_size, ls, distance, bs, beta, save_model):
+        super().__init__()
+        self.ls = ls # latent space
+        self.distance = distance
+        self.bs = bs # batch size
+        self.beta = beta # weight for distance term in loss function
+        self.save_model = save_model # true or false
         
-        np.random.seed(42)
-        set_random_seed(42)
-        # Build the encoder network
-        # ------------ Input -----------------
-        s1_inp = Input(shape=(self.args.s1_input_size,))
-        s2_inp = Input(shape=(self.args.s2_input_size,))
-        inputs = [s1_inp, s2_inp]
-
-        # ------------ Concat Layer -----------------
-        x1 = Dense(self.args.ds, activation=self.args.act)(s1_inp)
-        x1 = BN()(x1)
-
-        x2 = Dense(self.args.ds, activation=self.args.act)(s2_inp)
-        x2 = BN()(x2)
-
-        x = Concatenate(axis=-1)([x1, x2])
-
-        x = Dense(self.args.ds, activation=self.args.act)(x)
-        x = BN()(x)
-
-        # ------------ Embedding Layer --------------
-        z_mean = Dense(self.args.ls, name='z_mean')(x)
-        z_log_sigma = Dense(self.args.ls, name='z_log_sigma', kernel_initializer='zeros')(x)
-        z = Lambda(sampling, output_shape=(self.args.ls,), name='z')([z_mean, z_log_sigma])
-
-        self.encoder = Model(inputs, [z_mean, z_log_sigma, z], name='encoder')
-        self.encoder.summary()
-
-        # Build the decoder network
-        # ------------ Dense out -----------------
-        latent_inputs = Input(shape=(self.args.ls,), name='z_sampling')
-        x = latent_inputs
-        x = Dense(self.args.ds, activation=self.args.act)(x)
-        x = BN()(x)
+        # encoder
+        self.encoder.x1_fc = nn.Linear(x1_size, 128)
+        self.encoder.x2_fc = nn.Linear(x2_size, 128)
+        self.encoder.fuse = nn.Linear(128+128, 128)
+        # embedding
+        self.embed.mu = nn.Linear(128, ls)
+        self.embed.log_var = nn.Linear(128, ls)
+        # decoder
+        self.decoder.sample = nn.Linear(ls, 128)
+        self.decoder.x1_fc = nn.Linear(128, x1_size)
+        self.decoder.x2_fc = nn.Linear(128, x2_size)
         
-        x=Dropout(self.args.dropout)(x)
-        # ------------ Dense branches ------------
-        x1 = Dense(self.args.ds, activation=self.args.act)(x)
-        x1 = BN()(x1)
-        x2 = Dense(self.args.ds, activation=self.args.act)(x)
-        x2 = BN()(x2)
+    def sample_z(self, mu, log_var):
+        #Reparametrization Trick to allow gradients to backpropagate from the 
+        #stochastic part of the model
+        sigma = torch.exp(0.5*log_var)
+        z = torch.randn(size = (mu.size(0), mu.size(1)))
+        z= z.type_as(mu) # Setting z to be .cuda when using GPU training 
+        return mu + sigma*z
+    
+    def encode(self, x1, x2):
+        x1 = F.relu(self.encoder.x1_fc(x1))
+        x2 = F.relu(self.encoder.x2_fc(x2))
 
-        # ------------ Out -----------------------
-        s1_out = Dense(self.args.s1_input_size, activation='sigmoid')(x1)
+        x = torch.cat((x1, x2), dim=1)
+        x = F.relu(self.encoder.fuse(x))
+
+        mu = self.embed.mu(x)
+        log_var = self.embed.log_var(x)
+        return mu, log_var
+    
+    def decode(self, z):
+        x_hat = F.relu(self.decoder.sample(z))
+        x1_hat = F.relu(self.decoder.x1_fc(x_hat))
+        x2_hat = F.relu(self.decoder.x2_fc(x_hat))
+        return x1_hat, x2_hat
         
-        if self.args.integration == 'Clin+CNA':
-            s2_out = Dense(self.args.s2_input_size,activation='sigmoid')(x2)
-        else:
-            s2_out = Dense(self.args.s2_input_size)(x2)
+    def forward(self, x1, x2):
+        mu, log_var = self.encode(x1, x2)
+        z = self.sample_z(mu, log_var)
+        x1_hat, x2_hat = decode(z)
+        return x1_hat, x2_hat
+    
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3, betas=(0.9, 0.999), eps=1e-08, amsgrad=False, weight_decay=0.001)
+        # Using a scheduler is optional but can be helpful.
+        # The scheduler reduces the LR if the validation performance hasn't improved for the last N epochs
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.2, patience=20, min_lr=5e-5)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
+    
+    def compute_loss(self, batch):
+        x1, x2 = batch
+        mu, log_var = self.encode(x1, x2)
+        z = self.sample_z(mu, log_var)
+        x1_hat, x2_hat = decode(z)
 
-        decoder = Model(latent_inputs, [s1_out, s2_out], name='decoder')
-        decoder.summary()
-
-        outputs = decoder(self.encoder(inputs)[2])
-        self.vae = Model(inputs, outputs, name='vae_x')
-
-        if self.args.distance == "mmd":
-            true_samples = K.random_normal(K.stack([self.args.bs, self.args.ls]))
+        if self.distance == "mmd":
+            true_samples = torch.randn([self.bs, self.ls])
             distance = mmd(true_samples, z)
-        if self.args.distance == "kl":
-            distance = kl_regu(z_mean,z_log_sigma)
-          
+        if self.distance == "kld":
+            distance = kld(mu, log_var)         
+    
+        recon_loss_criterion = nn.MSELoss(reduction="mean")
+        recon_loss_x1 = recon_loss_criterion(x1, x1_hat)
+        recon_loss_x2 = recon_loss_criterion(x2, x2_hat)
+        recon_loss = recon_loss_x1 + recon_loss_x2
         
-        
-        s1_loss= binary_crossentropy(inputs[0], outputs[0])
+        vae_loss = recon_loss + self.beta * distance
+        return vae_loss
+    
+    def training_step(self, batch, batch_idx):
+        loss = self.compute_loss(batch)
+        self.log('train_loss', loss, on_step = True, on_epoch = True, prog_bar = True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        loss = self.compute_loss(batch)
+        self.log('val_loss', loss , on_step = True, on_epoch = True)
+        return x_out,loss
 
-        if self.args.integration == 'Clin+CNA':
-            s2_loss =binary_crossentropy(inputs[1], outputs[1])
-        else:
-            s2_loss =mean_squared_error(inputs[1], outputs[1])
-        
-        
-        reconstruction_loss = s1_loss+s2_loss
-        vae_loss = K.mean(reconstruction_loss + self.args.beta * distance)
-        self.vae.add_loss(vae_loss)
+    def test_step(self, batch, batch_idx):
+        loss = self.compute_loss(batch)
+        self.log('test_loss', loss , on_step = True, on_epoch = True)
+        return x_out,loss
 
-        adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, amsgrad=False, decay=0.001)
-        self.vae.compile(optimizer=adam, metrics=[mean_squared_error, mean_squared_error])
-        self.vae.summary()
+   
+def main():
+    # Initializing Dataloader
+    train_set = MNIST('data/',download = True,train = True,transform=data_transform)
+    train_loader = Dataloader(train_set,batch_size=64)
+    val_set = MNIST('data/',download = True,train = False,transform=data_transform)
+    val_loader = Dataloader(val_set,batch_size=64)
 
-    def train(self, s1_train, s2_train, s1_test, s2_test):
-        self.vae.fit([s1_train, s2_train], epochs=self.args.epochs, batch_size=self.args.bs, shuffle=True,
-                     validation_data=([s1_test, s2_test], None))
-        if self.args.save_model:
-            self.vae.save_weights('./models/vae_xvae.h5')
-
-    def predict(self, s1_data, s2_data):
-        return self.encoder.predict([s1_data, s2_data], batch_size=self.args.bs)[0]
+    model = XVAE(x1_size, x2_size, ls, distance, bs, beta, save_model)
+    # Initializing Trainer and setting parameters
+    trainer = Trainer(accelerator="auto", devices=1, max_epochs=25)
+    # Using trainer to fit vae model to dataset
+    trainer.fit(model, train_loader, val_loader)
+    
+    # Test best model on validation and test set
+    #val_result = trainer.test(model, dataloaders=val_loader, verbose=False)
+    #test_result = trainer.test(model, dataloaders=test_loader, verbose=False)
+    
+    
+if __name__ == "__main__":
+    main()
+    
+    
+    
