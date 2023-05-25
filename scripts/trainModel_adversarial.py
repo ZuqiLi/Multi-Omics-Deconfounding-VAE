88b40647fad1a30e9bac86158a36cf62ac33b179
sonja_mobile <katz@lifeglimmer.com>
2023-05-25 11:58:06 +0200

    extensively compared age deconfounding techniqes

--- scripts/trainModel_adversarial.py
+++ scripts/trainModel_adversarial.py
@@ -98,10 +98,10 @@ trainer_xvae  = L.Trainer(default_root_dir=os.getcwd(),
                     devices=1, 
                     log_every_n_steps=10, 
                     logger=logger_xvae, 
-                    max_epochs=1, ##10
+                    max_epochs=10, ##10
                     fast_dev_run=False) #
-#trainer_xvae.fit(model_pre_XVAE, train_loader, val_loader)
-#trainer_xvae.test(dataloaders=test_loader)
+# trainer_xvae.fit(model_pre_XVAE, train_loader, val_loader)
+# trainer_xvae.test(dataloaders=test_loader)
 
 
 ''' Step 2: pre-train adv net '''
@@ -118,18 +118,18 @@ trainer_advNet  = L.Trainer(default_root_dir=os.getcwd(),
                     devices=1, 
                     log_every_n_steps=10, 
                     logger=logger_advNet, 
-                    max_epochs=1, #10
+                    max_epochs=10, #10
                     fast_dev_run=False) #
-#trainer_advNet.fit(model_pre_advNet, train_loader, val_loader)
-#trainer_advNet.test(dataloaders=test_loader)
+# trainer_advNet.fit(model_pre_advNet, train_loader, val_loader)
+# trainer_advNet.test(dataloaders=test_loader)
 
 
 ''' Step 3A: train XVAE with adversarial loss '''
-ckpt_advNet_path = f"{os.getcwd()}/lightning_logs/advTraining/pre_advNet/pretrain_10epoch/checkpoints"
+ckpt_advNet_path = f"{os.getcwd()}/lightning_logs/advTraining/pre_advNet/version_0/checkpoints"
 ckpt_advNet_file = f"{ckpt_advNet_path}/{os.listdir(ckpt_advNet_path)[0]}"
 
 #### Train for only 1 epoch to have baseline to compare to and 50 times for actual comparison
-for epochs in [1, 50, 100]:
+for epochs in [300]:#[1, 50, 100]:
     model_xvae_adv = XVAE_w_advNet(PATH_xvae_ckpt=ckpt_xvae_file,
                                 PATH_advNet_ckpt=ckpt_advNet_file)
 
@@ -150,10 +150,10 @@ for epochs in [1, 50, 100]:
 
 ckpt_xvae_path = f"{os.getcwd()}/lightning_logs/advTraining/pre_XVAE/version_0/checkpoints"
 ckpt_xvae_file = f"{ckpt_xvae_path}/{os.listdir(ckpt_xvae_path)[0]}"
-ckpt_advNet_path = f"{os.getcwd()}/lightning_logs/advTraining/pre_advNet/pretrain_10epoch/checkpoints"
+ckpt_advNet_path = f"{os.getcwd()}/lightning_logs/advTraining/pre_advNet/version_0/checkpoints"
 ckpt_advNet_file = f"{ckpt_advNet_path}/{os.listdir(ckpt_advNet_path)[0]}"
 
-for epochs in [1,50,100]:
+for epochs in [300]:#[1, 50, 100]:
     model_xvae_adv = XVAE_w_advNet_pingpong(PATH_xvae_ckpt=ckpt_xvae_file,
                                             PATH_advNet_ckpt=ckpt_advNet_file,
                                             lamdba_deconf = 1)
