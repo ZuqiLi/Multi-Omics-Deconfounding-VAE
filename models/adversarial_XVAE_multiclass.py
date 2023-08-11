test on categorical confounder (weight range [0,1]); remove Softmax layer for advNet; fix the problem of having different path for adversarial models

models/adversarial_XVAE_multiclass.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 import pytorch_lightning as L
 from sklearn.metrics import mean_absolute_error, roc_auc_score
 from scipy.stats import pearsonr
-from models.func import kld, mmd, reconAcc_pearsonCorr, reconAcc_relativeError, crossEntropy, bce, init_weights, mse
+from models.func import kld, mmd, reconAcc_pearsonCorr, reconAcc_relativeError, crossEntropy, nll, bce, init_weights, mse
 from models.clustering import *
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -49,7 +49,7 @@ class XVAE_scGAN_multiclass(L.LightningModule):
                             *self.xvae.enc_hidden_fused)
 
         ### Load pre-trained advNet and freeze weights
-        self.advNet_pre = advNet.load_from_checkpoint(PATH_advNet_ckpt)
+        self.advNet_pre = advNet.load_from_checkpoint(PATH_advNet_ckpt, PATH_xvae_ckpt=PATH_xvae_ckpt)
 
 
 
@@ -151,7 +151,7 @@ class XVAE_adversarial_multiclass(L.LightningModule):
         self.xvae = XVAE.load_from_checkpoint(PATH_xvae_ckpt)
 
         ### Load pre-trained advNet and freeze weights
-        self.advNet = advNet.load_from_checkpoint(PATH_advNet_ckpt)
+        self.advNet_pre = advNet.load_from_checkpoint(PATH_advNet_ckpt, PATH_xvae_ckpt=PATH_xvae_ckpt)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(filter(lambda p: p.requires_grad, self.parameters()), lr=1e-3, betas=(0.9, 0.999), eps=1e-08, amsgrad=False, weight_decay=0)
@@ -168,7 +168,7 @@ class XVAE_adversarial_multiclass(L.LightningModule):
         ''' 
         Adversarial net loss
         ''' 
-        regr_loss, clf_loss = self.advNet.compute_loss(batch)
+        regr_loss, clf_loss = self.advNet_pre.compute_loss(batch)
         advNet_loss = regr_loss + clf_loss 
 
         '''
@@ -184,7 +184,7 @@ class XVAE_adversarial_multiclass(L.LightningModule):
         ''' 
         Adversarial net loss
         ''' 
-        regr_loss, clf_loss = self.advNet.compute_loss(batch)
+        regr_loss, clf_loss = self.advNet_pre.compute_loss(batch)
         advNet_loss = regr_loss + clf_loss 
         return advNet_loss
 
@@ -193,7 +193,7 @@ class XVAE_adversarial_multiclass(L.LightningModule):
         if self.current_epoch % 2 == 0:
             for param in self.xvae.parameters():
                 param.requires_grad = True
-            for param in self.advNet.parameters():
+            for param in self.advNet_pre.parameters():
                 param.requires_grad = False        
             # Calculate loss
             ae_loss, advNet_loss, combined_loss = self.compute_loss_combined(batch)
@@ -206,7 +206,7 @@ class XVAE_adversarial_multiclass(L.LightningModule):
             # Freeze the autoencoder
             for param in self.xvae.parameters():
                 param.requires_grad = False
-            for param in self.advNet.parameters():
+            for param in self.advNet_pre.parameters():
                 param.requires_grad = True        
             advNet_loss = self.compute_loss_advNet(batch)
             # save metric
@@ -244,7 +244,7 @@ class XVAE_adversarial_1batch_multiclass(L.LightningModule):
         self.xvae = XVAE.load_from_checkpoint(PATH_xvae_ckpt)
 
         ### Load pre-trained advNet and freeze weights
-        self.advNet_pre = advNet.load_from_checkpoint(PATH_advNet_ckpt)
+        self.advNet_pre = advNet.load_from_checkpoint(PATH_advNet_ckpt, PATH_xvae_ckpt=PATH_xvae_ckpt)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(filter(lambda p: p.requires_grad, self.parameters()), lr=1e-3, betas=(0.9, 0.999), eps=1e-08, amsgrad=False, weight_decay=0)
@@ -352,8 +352,9 @@ class advNet(L.LightningModule):
 
         if self.num_cov_clf:
             if self.num_cov_clf > 1:       
-                self.adv_net_clf = nn.Sequential(nn.Linear(10,  self.num_cov_clf),
-                                                nn.Softmax())     
+                self.adv_net_clf = nn.Sequential(nn.Linear(10,  self.num_cov_clf))
+                #self.adv_net_clf = nn.Sequential(nn.Linear(10,  self.num_cov_clf),
+                #                                nn.Softmax())
                 self.loss_func_clf = "crossEntropy"
             else: 
                 self.adv_net_clf = nn.Sequential(nn.Linear(10,  self.num_cov_clf),
@@ -400,8 +401,10 @@ class advNet(L.LightningModule):
             cov_clf = cov_clf.to(torch.float32)
             if self.loss_func_clf == "crossEntropy":
                 clfloss = crossEntropy(y_pred_clf, cov_clf)    
+                #cov_clf = torch.argmax(cov_clf, dim=1) # input for NLL loss should be 1D
+                #clfloss = nll(y_pred_clf, cov_clf)    
             else:
-                clfloss = bce(y_pred_clf, cov_clf)              
+                clfloss = bce(y_pred_clf, cov_clf)
         else: clfloss = 0        
         return regrloss, clfloss
 
@@ -419,7 +422,7 @@ class advNet(L.LightningModule):
         self.log('advNet_val_loss', loss, on_step = False, on_epoch = True)            
         return loss
     
-    
+  
 
 class XVAE(L.LightningModule):
     def __init__(self, 

scripts/XVAE/trainModel.py
@@ -26,8 +26,8 @@ PATH_data = "Data"
 
 
 ''' Load data '''
-X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ.csv'), delimiter=",")
-X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ.csv'), delimiter=",")
+X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ2.csv'), delimiter=",")
+X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ2.csv'), delimiter=",")
 X1 = torch.from_numpy(X1).to(torch.float32)
 X2 = torch.from_numpy(X2).to(torch.float32)
 traits = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_clinic2.csv'), delimiter=",", skiprows=1, usecols=(1,2,3,4,5))
@@ -51,7 +51,7 @@ conf = conf[:,[0]]
 '''
 # load artificial confounder
 conf_type = 'categ'
-conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ.csv'))[:,None]
+conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ2.csv'))[:,None]
 conf = torch.from_numpy(conf).to(torch.float32)
 if conf_type == 'categ':
     conf = torch.nn.functional.one_hot(conf[:,0].to(torch.int64))
@@ -85,7 +85,7 @@ val_loader = data.DataLoader(
 #################################################
 ##             Training procedure              ##
 #################################################
-modelname = 'confounded_categ/XVAE'
+modelname = 'confounded_categ2/XVAE'
 maxEpochs = 150
 
 for epoch in [1, maxEpochs]:

scripts/XVAE/trainModel_removeLatFeatures.py
@@ -28,8 +28,8 @@ PATH_data = "Data"
 
 
 ''' Load data '''
-X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ.csv'), delimiter=",")
-X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ.csv'), delimiter=",")
+X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ2.csv'), delimiter=",")
+X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ2.csv'), delimiter=",")
 X1 = torch.from_numpy(X1).to(torch.float32)
 X2 = torch.from_numpy(X2).to(torch.float32)
 traits = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_clinic2.csv'), delimiter=",", skiprows=1, usecols=(1,2,3,4,5))
@@ -53,7 +53,7 @@ conf = conf[:,[0]]
 '''
 # load artificial confounder
 conf_type = 'categ'
-conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ.csv'))[:,None]
+conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ2.csv'))[:,None]
 conf = torch.from_numpy(conf).to(torch.float32)
 if conf_type == 'categ':
     conf = torch.nn.functional.one_hot(conf[:,0].to(torch.int64))
@@ -87,7 +87,8 @@ val_loader = data.DataLoader(
 #################################################
 ##             Training procedure              ##
 #################################################
-modelname = 'confounded_categ/XVAE'
+modelname = 'confounded_categ2/XVAE'
+modelname = 'XVAE'
 maxEpochs = 150
 
 
@@ -143,14 +144,14 @@ for i in range(50):
             Disadvantage?: number of latent features to keep are different for every consensus run 
             '''
             print(f"\nDimension latent space - before: {z.shape}")
-            cutoff_corr = 0.3
-            #cutoff_pvalue = 0.05
+            #cutoff_corr = 0.5
+            cutoff_pvalue = 0.05
             for i in range(conf.shape[1]):
                 ## determine correlation of all latFeatures to one confounder variable
-                corr = np.array([abs(pearsonr(z[:,j], conf[:,i])[0]) for j in range(z.shape[1])])
-                tmp = corr < cutoff_corr
-                #corr_pval = np.array([pearsonr(z[:,j], conf[:,i])[1] for j in range(z.shape[1])])
-                #tmp = corr_pval > cutoff_pvalue
+                #corr = np.array([abs(pearsonr(z[:,j], conf[:,i])[0]) for j in range(z.shape[1])])
+                #tmp = corr < cutoff_corr
+                corr_pval = np.array([pearsonr(z[:,j], conf[:,i])[1] for j in range(z.shape[1])])
+                tmp = corr_pval > cutoff_pvalue
                 if i == 0:
                     booleanCorr = tmp.copy()
                 else:

scripts/XVAE_corrReg/trainModel_corrReg.py
@@ -14,7 +14,7 @@ from Data.preprocess import *
 from models.func import reconAcc_relativeError
 
 
-os.environ["CUDA_VISIBLE_DEVICES"]=""
+#os.environ["CUDA_VISIBLE_DEVICES"]=""
 
 ''' Set seeds for replicability  -Ensure that all operations are deterministic on GPU (if used) for reproducibility '''
 np.random.seed(1234)
@@ -28,8 +28,8 @@ PATH_data = "Data"
 
 
 ''' Load data '''
-X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ.csv'), delimiter=",")
-X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ.csv'), delimiter=",")
+X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ2.csv'), delimiter=",")
+X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ2.csv'), delimiter=",")
 X1 = torch.from_numpy(X1).to(torch.float32)
 X2 = torch.from_numpy(X2).to(torch.float32)
 traits = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_clinic2.csv'), delimiter=",", skiprows=1, usecols=(1,2,3,4,5))
@@ -53,7 +53,7 @@ conf = conf[:,[0]]
 '''
 # load artificial confounder
 conf_type = 'linear'
-conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ.csv'))[:,None]
+conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ2.csv'))[:,None]
 conf = torch.from_numpy(conf).to(torch.float32)
 if conf_type == 'categ':
     conf = torch.nn.functional.one_hot(conf[:,0].to(torch.int64))
@@ -87,7 +87,7 @@ val_loader = data.DataLoader(
 #################################################
 ##             Training procedure              ##
 #################################################
-modelname = 'confounded_categ/XVAE_corrReg/XVAE_corrReg_MIhist'
+modelname = 'confounded_categ2/XVAE_corrReg/XVAE_corrReg_corrSq'
 maxEpochs = 150
 
 for epoch in [1, maxEpochs]:
@@ -100,7 +100,7 @@ for epoch in [1, maxEpochs]:
                 ls=50,                      # latent size
                 cov_size=conf.shape[1],     # number of covariates
                 distance='mmd',             # variational term: KL divergence or maximum mean discrepancy
-                corrReg='MIhist',           # correlation regularization: corrAbs, corrSq, MIhist, MIkde
+                corrReg='corrSq',           # correlation regularization: corrAbs, corrSq, MIhist, MIkde
                 lossReduction='sum',        # sum or mean reduction for loss terms
                 klAnnealing=False,          # annealing for KL vanishing
                 beta=1,                     # weight for variational term

scripts/adversarial_XVAE/trainModel_VAE_adversarial_multiclass.py
@@ -12,7 +12,7 @@ from models.clustering import *
 from Data.preprocess import *
 from models.func import reconAcc_relativeError
 ### Specify here which model you want to use: XVAE_adversarial_multiclass, XVAE_scGAN_multiclass, XVAE_adversarial_1batch_multiclass
-from models.adversarial_XVAE_multiclass import XVAE, advNet, XVAE_scGAN_multiclass
+from models.adversarial_XVAE_multiclass import XVAE, advNet, XVAE_adversarial_multiclass
 
 
 ''' Set seeds for replicability  -Ensure that all operations are deterministic on GPU (if used) for reproducibility '''
@@ -27,8 +27,8 @@ PATH_data = "Data"
 
 
 ''' Load data '''
-X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ.csv'), delimiter=",")
-X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ.csv'), delimiter=",")
+X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ2.csv'), delimiter=",")
+X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ2.csv'), delimiter=",")
 X1 = torch.from_numpy(X1).to(torch.float32)
 X2 = torch.from_numpy(X2).to(torch.float32)
 traits = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_clinic2.csv'), delimiter=",", skiprows=1, usecols=(1,2,3,4,5))
@@ -52,7 +52,7 @@ conf = conf[:,[0]]
 '''
 # load artificial confounder
 conf_type = 'categ'
-conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ.csv'))[:,None]
+conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ2.csv'))[:,None]
 conf = torch.from_numpy(conf).to(torch.float32)             ### Watch out: continous variables should go first
 if conf_type == 'categ':
     conf = torch.nn.functional.one_hot(conf[:,0].to(torch.int64))
@@ -92,7 +92,7 @@ val_loader = data.DataLoader(
 #################################################
 ##             Training procedure              ##
 #################################################
-modelname = "confounded_categ/XVAE_advTraining_scGAN_multiclass"
+modelname = "confounded_categ2/XVAE_advTraining/XVAE_advTraining_multiclass"
 ls = 50
 
 ## pretrainig epochs
@@ -156,8 +156,7 @@ trainer_advNet = L.Trainer(default_root_dir=os.getcwd(),
                     fast_dev_run=False,
                     deterministic=True)
 trainer_advNet.fit(model_pre_advNet, train_loader, val_loader)
-os.rename(f"lightning_logs/{modelname}/pre_advNet/version_0", f"lightning_logs/{modelname}/pre_advNet/epoch{epochs_preTrg_ae}")
-
+os.rename(f"lightning_logs/{modelname}/pre_advNet/version_0", f"lightning_logs/{modelname}/pre_advNet/epoch{epochs_preTrg_advNet}")
 
 
 ''' 
@@ -169,11 +168,9 @@ ckpt_advNet_path = f"{os.getcwd()}/lightning_logs/{modelname}/pre_advNet/epoch{e
 ckpt_advNet_file = f"{ckpt_advNet_path}/{os.listdir(ckpt_advNet_path)[0]}"
 
 
-
 for epoch in [1, epochs_ae_w_advNet]:
 
-
-    model_xvae_adv = XVAE_scGAN_multiclass(PATH_xvae_ckpt=ckpt_xvae_file,
+    model_xvae_adv = XVAE_adversarial_multiclass(PATH_xvae_ckpt=ckpt_xvae_file,
                                                 PATH_advNet_ckpt=ckpt_advNet_file,
                                                 lamdba_deconf = 1,
                                                 labels_onehot = labels_onehot)
@@ -193,7 +190,6 @@ for epoch in [1, epochs_ae_w_advNet]:
 
 
 
-
 ###############################################
 ##         Test on the whole dataset         ##
 ###############################################
@@ -208,14 +204,52 @@ clusts = []
 SSs, DBs = [], []
 n_clust = len(np.unique(Y))
 corr_diff = []
+
+## advNet performance for predicting the confounder
+from sklearn.metrics import roc_auc_score
+scores = np.zeros((50, conf_test.shape[1]))
+for i in range(50):         
+    ckpt_path = f"{os.getcwd()}/lightning_logs/{modelname}/XVAE_adversarialTrg/epoch{epochs_ae_w_advNet}/checkpoints"
+    ckpt_file = f"{ckpt_path}/{os.listdir(ckpt_path)[0]}"
+    ckpt_xvae_path = f"{os.getcwd()}/lightning_logs/{modelname}/pre_XVAE/epoch{epochs_preTrg_ae}/checkpoints"
+    ckpt_xvae_file = f"{ckpt_xvae_path}/{os.listdir(ckpt_xvae_path)[0]}"
+    ckpt_advNet_path = f"{os.getcwd()}/lightning_logs/{modelname}/pre_advNet/epoch{epochs_preTrg_advNet}/checkpoints"
+    ckpt_advNet_file = f"{ckpt_advNet_path}/{os.listdir(ckpt_advNet_path)[0]}"
+
+    model = XVAE_adversarial_multiclass.load_from_checkpoint(ckpt_file,
+            PATH_xvae_ckpt=ckpt_xvae_file, PATH_advNet_ckpt=ckpt_advNet_file)
+    
+    # Loop over dataset and test on batches
+    indices = np.array_split(np.arange(X1_test.shape[0]), 20)
+    y_pred_all, y_true_all = [], []
+    for idx in indices:
+        _, y_pred = model.advNet_pre.forward(X1_test[idx], X2_test[idx])
+        y_pred = [y_pred[j].detach().numpy() for j in range(y_pred.shape[0])]
+        y_pred_all.append(y_pred)
+
+    y_pred_all = np.concatenate(y_pred_all)
+
+    for j in range(conf_test.shape[1]):
+        scores[i,j] = roc_auc_score(conf_test[:,j], y_pred_all[:,j])
+
+scores = np.mean(scores, 0)
+for i in range(conf_test.shape[1]):
+    print(f"Conf{i+1}", "\t", round(scores[i], 2))
+
+## Compute reconstruction and clustering metrics
 # Sample multiple times from the latent distribution for stability
 for i in range(50):         
     corr_res = []
     for epoch in [1, epochs_ae_w_advNet]:
         ckpt_path = f"{os.getcwd()}/lightning_logs/{modelname}/XVAE_adversarialTrg/epoch{epoch}/checkpoints"
         ckpt_file = f"{ckpt_path}/{os.listdir(ckpt_path)[0]}"
+        ckpt_xvae_path = f"{os.getcwd()}/lightning_logs/{modelname}/pre_XVAE/epoch{epochs_preTrg_ae}/checkpoints"
+        ckpt_xvae_file = f"{ckpt_xvae_path}/{os.listdir(ckpt_xvae_path)[0]}"
+        ckpt_advNet_path = f"{os.getcwd()}/lightning_logs/{modelname}/pre_advNet/epoch{epochs_preTrg_advNet}/checkpoints"
+        ckpt_advNet_file = f"{ckpt_advNet_path}/{os.listdir(ckpt_advNet_path)[0]}"
 
-        model = XVAE_scGAN_multiclass.load_from_checkpoint(ckpt_file)
+        model = XVAE_adversarial_multiclass.load_from_checkpoint(ckpt_file,
+                PATH_xvae_ckpt=ckpt_xvae_file, PATH_advNet_ckpt=ckpt_advNet_file)
         
         # Loop over dataset and test on batches
         indices = np.array_split(np.arange(X1_test.shape[0]), 20)

scripts/cXVAE/trainModel_cXVAE.py
@@ -8,7 +8,7 @@ from pytorch_lightning.loggers import TensorBoardLogger
 import torch.utils.data as data
 import sys
 sys.path.append("./")
-from models.cXVAE import cXVAE_fusedEmbed # cXVAE_input, cXVAE_inputEmbed, cXVAE_embed, cXVAE_fusedEmbed
+from models.cXVAE import cXVAE_embed # cXVAE_input, cXVAE_inputEmbed, cXVAE_embed, cXVAE_fusedEmbed
 from models.clustering import *
 from Data.preprocess import *
 from models.func import reconAcc_relativeError
@@ -26,8 +26,8 @@ PATH_data = "Data"
 
 
 ''' Load data '''
-X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ.csv'), delimiter=",")
-X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ.csv'), delimiter=",")
+X1 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_mRNA2_confounded_categ2.csv'), delimiter=",")
+X2 = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_DNAm_confounded_categ2.csv'), delimiter=",")
 X1 = torch.from_numpy(X1).to(torch.float32)
 X2 = torch.from_numpy(X2).to(torch.float32)
 traits = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_clinic2.csv'), delimiter=",", skiprows=1, usecols=(1,2,3,4,5))
@@ -51,7 +51,7 @@ conf = conf[:,[0]]
 '''
 # load artificial confounder
 conf_type = 'categ'
-conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ.csv'))[:,None]
+conf = np.loadtxt(os.path.join(PATH_data, "TCGA",'TCGA_confounder_categ2.csv'))[:,None]
 conf = torch.from_numpy(conf).to(torch.float32)
 if conf_type == 'categ':
     conf = torch.nn.functional.one_hot(conf[:,0].to(torch.int64))
@@ -84,12 +84,12 @@ val_loader = data.DataLoader(
 #################################################
 ##             Training procedure              ##
 #################################################
-modelname = 'confounded_categ/cXVAE/cXVAE_fusedEmbed'
+modelname = 'confounded_categ2/cXVAE/cXVAE_embed'
 maxEpochs = 150
 
 for epoch in [1, maxEpochs]:
     # Initialize model
-    model = cXVAE_fusedEmbed(input_size = [X1.shape[1], X2.shape[1]],
+    model = cXVAE_embed(input_size = [X1.shape[1], X2.shape[1]],
                 # first hidden layer: individual encoding of X1 and X2; [layersizeX1, layersizeX2]; length: number of input modalities
                 hidden_ind_size =[200, 200],
                 # next hidden layer(s): densely connected layers of fused X1 & X2; [layer1, layer2, ...]; length: number of hidden layers
@@ -138,7 +138,7 @@ for i in range(50):
         ckpt_path = f"{os.getcwd()}/lightning_logs/{modelname}/epoch{epoch}/checkpoints"
         ckpt_file = f"{ckpt_path}/{os.listdir(ckpt_path)[0]}"
 
-        model = cXVAE_fusedEmbed.load_from_checkpoint(ckpt_file)
+        model = cXVAE_embed.load_from_checkpoint(ckpt_file)
         
         # Loop over dataset and test on batches
         indices = np.array_split(np.arange(X1_test.shape[0]), 20)